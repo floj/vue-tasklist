71399b2cb3478852098bab2b8a62c742694af3dc
floj <floj@users.noreply.github.com>
2024-10-15 16:57:34 +0200

    refactor to use classes for state, also add emojis

--- main.js
+++ main.js
@@ -1,121 +1,178 @@
 import { createApp } from "https://cdn.jsdelivr.net/npm/vue@3.5.12/dist/vue.esm-browser.js";
 
-function loadLists() {
-  const v = localStorage.getItem("lists");
-  if (!v) {
-    return [{ name: "Meine Liste", selected: true }];
-  }
-  return JSON.parse(v);
-}
-
-function saveLists(lists) {
-  const v = JSON.stringify(lists);
-  localStorage.setItem("lists", v);
-}
-
-function loadTasks(listName) {
-  if (!listName) {
-    return [];
-  }
-  const v = localStorage.getItem(`list-${listName}`);
-  if (!v) {
-    return [];
-  }
-  const tasks = JSON.parse(v);
-  tasks.forEach((t) => {
-    t.created = new Date(t.created);
-    t.updated = new Date(t.updated);
-  });
-  return tasks;
-}
-
-function saveTasks(listName, tasks) {
-  const v = JSON.stringify(tasks);
-  localStorage.setItem(`list-${listName}`, v);
-}
-
 let ids = 0;
+
+class Tasklist {
+  id;
+  /** @type {Array<Task>} */
+  tasks = [];
+  name;
+  created;
+  updated;
+
+  /**
+   * @param {string} name
+   */
+  constructor(name) {
+    const now = Date.now();
+    this.id = `${now}-${ids++}`;
+    this.created = now;
+    this.updated = now;
+    this.name = name;
+  }
+}
+
+class Task {
+  id;
+  title;
+  emoji;
+  completed = false;
+  created;
+  updated;
+
+  /**
+   * @param {string} title
+   */
+  constructor(title) {
+    const now = Date.now();
+    this.id = `${now}-${ids++}`;
+    this.created = now;
+    this.updated = now;
+    this.title = title;
+    this.emoji = "";
+  }
+
+  restore() {
+    ["created", "updated"].forEach((prop) => {
+      if (typeof this[prop] === "string") {
+        this[prop] = new Date(this[prop]);
+      }
+    });
+  }
+}
+
+class State {
+  /** @type {Array<Tasklist>} */
+  lists;
+  /** @type {string} */
+  currentList;
+}
+
+function saveState(state) {
+  const v = JSON.stringify(state);
+  localStorage.setItem("taskState", v);
+}
+
+function restoreState() {
+  const v = localStorage.getItem("taskState");
+  if (v) {
+    const state = JSON.parse(v);
+    state.lists.forEach((l) => {
+      l.created = new Date(l.created);
+      l.updated = new Date(l.updated);
+    });
+    return state;
+  }
+  const initialState = new State();
+  const initialList = new Tasklist("Meine Liste");
+  initialState.lists = [initialList];
+  initialState.currentList = initialList.id;
+  return initialState;
+}
+
+let emojis = [];
+
 createApp({
   data() {
-    const lists = loadLists();
-    const currentList = lists.find((l) => l.selected)?.name;
-    const tasks = loadTasks(currentList);
+    const state = restoreState();
     return {
-      lists,
-      tasks,
+      state,
       newTask: "",
       newList: "",
     };
   },
   mounted() {
     this.$refs.newTaskEl.focus();
+    this.loadEmojis();
   },
   computed: {
-    currentList() {
-      return this.lists.find((l) => l.selected).name;
+    selectedList() {
+      return this.state.lists.find((l) => l.id == this.state.currentList);
     },
-    allTasks() {
-      return this.tasks.toSorted((left, right) => {
-        if (left.completed && !right.completed) {
+    currentTasks() {
+      return this.selectedList.tasks.toSorted((left, right) => {
+        if (left.completed === right.completed) {
+          return right.updated - left.updated;
+        }
+        if (left.completed) {
           return 1;
         }
-        if (!left.completed && right.completed) {
-          return -1;
-        }
-        // else order by last updated first
-        return right.updated.getTime() - left.updated.getTime();
+        return -1;
       });
+    },
+    hasCompletedTasks() {
+      return this.currentTasks.find((t) => t.completed) != null;
     },
   },
   watch: {
-    tasks: {
+    state: {
       handler() {
-        saveTasks(this.currentList, this.tasks);
+        saveState(this.state);
       },
       deep: true,
     },
   },
   methods: {
-    toggleCompleted(task) {
-      task.completed = !task.completed;
-      task.updated = new Date();
-    },
-    addTask() {
-      if (!this.newTask) {
-        return;
-      }
-      const now = new Date();
-      this.tasks.push({
-        name: this.newTask,
-        completed: false,
-        id: `${now.getTime()}-${ids++}`,
-        created: now,
-        updated: now,
-      });
-      this.newTask = "";
-    },
-    removeTask(task) {
-      this.tasks = this.tasks.filter((t) => t.id != task.id);
+    loadEmojis() {
+      fetch(
+        "https://cdn.jsdelivr.net/npm/@kazvmoe-infra/unicode-emoji-json@0.4.0/annotations/de.json"
+      )
+        .then((resp) => resp.json())
+        .then((json) => {
+          Object.entries(json).forEach(([symbol, v]) => {
+            emojis.push({
+              symbol,
+              ...v,
+            });
+          });
+        });
     },
     addList() {
       if (!this.newList) {
         return;
       }
-      this.lists.forEach((l) => (l.selected = false));
-      this.lists.push({
-        name: this.newList,
-        selected: true,
-      });
-
-      saveLists(this.lists);
-
-      this.tasks = loadTasks(this.newList);
+      const newList = new Tasklist(this.newList);
+      this.state.lists.push(newList);
+      this.state.currentList = newList.id;
       this.newList = "";
     },
-    changeList(listName) {
-      this.lists.forEach((l) => (l.selected = l.name == listName));
-      this.tasks = loadTasks(listName);
-      saveLists(this.lists);
+    addTask() {
+      if (!this.newTask) {
+        return;
+      }
+      const task = new Task(this.newTask);
+
+      const emoji = emojis.find((e) => e.keywords.includes(this.newTask));
+      if (emoji) {
+        task.emoji = emoji.symbol;
+      }
+
+      this.selectedList.tasks.push(task);
+      this.newTask = "";
+    },
+    removeTask(task) {
+      this.selectedList.tasks = this.selectedList.tasks.filter(
+        (t) => t.id != task.id
+      );
+    },
+    toggleCompleted(task) {
+      task.completed = !task.completed;
+      task.updated = new Date();
+    },
+    removeCompletedTasks() {
+      this.selectedList.tasks = this.selectedList.tasks.filter(
+        (t) => !t.completed
+      );
     },
   },
 }).mount("#app");
